d0100cc275aefd7f820cd760d51789a8d11d2867
Sudhanshu Panthri <panthrisudhanshu666@gmail.com>
2021-08-08 20:19:26 +0530

    Inital Commit

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -1,10 +1,27 @@
+import axios from '../../axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import VideoCard from '../VideoCard/VideoCard'
 import './Results.css'
+import requests from '../../requests'
 function Results() {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchActionMovies)
+            // console.log(request);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+    }, [])
     return (
         <div className='results'>
-            <VideoCard />
+            {movies.map((movie) => (
+                <VideoCard movie={movie} />
+            ))}
         </div>
     )
 }
--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './VideoCard.css'
+import TextTruncate from 'react-text-truncate'
+import { ThumbUpSharp } from '@material-ui/icons'
 
-function VideoCard(props) {
+const baseUrl = "https://image.tmdb.org/t/p/original/"
+
+function VideoCard({ movie }) {
     return (
         <div className='videoCard'>
-            <h4>VideoCard</h4>
+            <div className="imgContainer">
+                <img src={`${baseUrl}${movie.backdrop_path || movie.poster_path}`} alt={movie.title} />
+            </div>
+            <TextTruncate
+                line={1}
+                element="p"
+                truncateText="..."
+                // textTruncateChild={<a href="#">Read more</a>}
+                text={movie.overview} />
+            <h2>{movie.title}</h2>
+            <p className='videoCard_Stats'>
+                {movie.media_type && `${movie.media_type} :`}
+                {movie.release_date || movie.first_air_date}
+                <span>{` - `}</span>
+                <ThumbUpSharp />{movie.vote_count}</p>
         </div>
     )
 }
